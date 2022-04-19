570d85697ed61361192f85a583e5f9f318283535
shamagir <shamagir@gmail.com>
2022-04-20 02:59:31 +0300

    fix: add ideas of functionality

--- db.php
+++ db.php
@@ -64,8 +64,8 @@ function addVideo($video_id)
     $table_name = $wpdb->prefix . "videocourse";
     $current_user = wp_get_current_user();
     $uid = $current_user->ID;
-    //check if there is a course in the user's list
     //get the course id by video id
+    //check if there is a course in the user's list
     //addAllVideos($course_id)
     //check if recordings exist
     $result = $wpdb->get_results("SELECT * FROM $table_name WHERE `user_id` = $uid AND 'post_id' = $video_id");
