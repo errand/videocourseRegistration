aa8707c7fc0b46636cced20cbd45e28c9f2c8e39
Aleksandr Shatskikh <work@errand.ru>
2022-05-05 20:48:59 +0300

    feat: hide admin bar for subscribers

--- videocourseRegistration.php
+++ videocourseRegistration.php
@@ -190,3 +190,25 @@ function my_login_logo_url() {
   return '/videocourses';
 }
 add_filter( 'login_headerurl', 'my_login_logo_url' );
+
+function tf_check_user_role( $roles ) {
+  /*@ Check user logged-in */
+  if ( is_user_logged_in() ) :
+    /*@ Get current logged-in user data */
+    $user = wp_get_current_user();
+    /*@ Fetch only roles */
+    $currentUserRoles = $user->roles;
+    /*@ Intersect both array to check any matching value */
+    $isMatching = array_intersect( $currentUserRoles, $roles);
+    $response = false;
+    /*@ If any role matched then return true */
+    if ( !empty($isMatching) ) :
+      $response = true;
+    endif;
+    return $response;
+  endif;
+}
+$roles = [ 'customer', 'subscriber' ];
+if ( tf_check_user_role($roles) ) :
+  add_filter('show_admin_bar', '__return_false');
+endif;
