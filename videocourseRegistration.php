4e33ad2383d3214683547774b3a60d0c074b787a
Aleksandr Shatskikh <work@errand.ru>
2022-05-25 16:06:28 +0300

    feat: add mail notification

--- videocourseRegistration.php
+++ videocourseRegistration.php
@@ -288,15 +288,17 @@ function send_welcome_email_to_new_user($user_id) {
   $user = get_userdata($user_id);
   $user_email = $user->user_email;
   // for simplicity, lets assume that user has typed their first and last name when they sign up
-  //$user_full_name = $user->user_firstname . $user->user_lastname;
+  $user_full_name = $user->user_firstname . $user->user_lastname;
 
   $subjectField = get_field('notification_subject', 'option');
   $bodyField = get_field('notification_body', 'option');
 
   // Now we are ready to build our welcome email
   $to = $user_email;
-  $subject = $subjectField;
-  $body = $bodyField;
+  $subject = "" . $subjectField . "";
+  $body = '
+              '.$bodyField.'
+    ';
   $headers = array('Content-Type: text/html; charset=UTF-8');
   if (wp_mail($to, $subject, $body, $headers)) {
     error_log("email has been successfully sent to user whose email is " . $user_email);
