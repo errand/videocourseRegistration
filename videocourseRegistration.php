e66122ceb724634309a941114760b2b3bd030ab1
Aleksandr Shatskikh <work@errand.ru>
2022-05-25 13:10:47 +0300

    feat: add mail notification

--- videocourseRegistration.php
+++ videocourseRegistration.php
@@ -52,7 +52,6 @@ function registerUser()
 
 function auto_login_new_user($user_id)
 {
-    wp_send_new_user_notifications($user_id);
     wp_set_current_user($user_id);
     wp_set_auth_cookie($user_id);
     $user = get_user_by('id', $user_id);
@@ -281,3 +280,36 @@ add_filter('manage_' . $menu_order_sortable_on_screen . '_sortable_columns', fun
   $columns['menu_order'] = 'menu_order';
   return $columns;
 });
+
+
+// Sent user notification email
+
+function send_welcome_email_to_new_user($user_id) {
+  $user = get_userdata($user_id);
+  $user_email = $user->user_email;
+  // for simplicity, lets assume that user has typed their first and last name when they sign up
+  $user_full_name = $user->user_firstname . $user->user_lastname;
+
+  // Now we are ready to build our welcome email
+  $to = $user_email;
+  $subject = "Hi " . $user_full_name . ", welcome to our site!";
+  $body = '
+              <h1>Dear ' . $user_full_name . ',</h1></br>
+              <p>Thank you for joining our site. Your account is now active.</p>
+              <p>Please go ahead and navigate around your account.</p>
+              <p>Let me know if you have further questions, I am here to help.</p>
+              <p>Enjoy the rest of your day!</p>
+              <p>Kind Regards,</p>
+              <p>poanchen</p>
+    ';
+  $headers = array('Content-Type: text/html; charset=UTF-8');
+  if (wp_mail($to, $subject, $body, $headers)) {
+    error_log("email has been successfully sent to user whose email is " . $user_email);
+  }else{
+    error_log("email failed to sent to user whose email is " . $user_email);
+  }
+}
+
+// THE ONLY DIFFERENCE IS THIS LINE
+add_action('user_register', 'send_welcome_email_to_new_user');
+// THE ONLY DIFFERENCE IS THIS LINE
